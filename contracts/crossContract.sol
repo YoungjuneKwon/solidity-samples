00d36aca20ec2355b0e4d171bf0f9a1c022c1e71
Youngjune Kwon <yjkwon@winm2m.com>
2022-09-30 11:09:16 +0900

    Update crossContract.sol

--- contracts/crossContract.sol
+++ contracts/crossContract.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract MyCrocessContract {
+    IERC721 other;
+    uint256 pA;
+    uint256 pB;
+
+    constructor(address otherAddress, uint256 priceA, uint256 priceB) {
+        other = IERC721(otherAddress);
+        pA = priceA;
+        pB = priceB;
+    }
+
+    function getPrice() public view returns (uint256) {
+        uint256 b = other.balanceOf(msg.sender);
+        return b > 0 ? pB : pA;
+    }
+}
